7b4026a53aadfdc77a2318587512b8e0f0ca174c
ZNWL <znwinlwin@gmail.com>
2022-01-11 13:45:03 +0630

    From ZNWL

--- food_lab/resources/views/COMMON/layout/layout_admin.blade.php
+++ food_lab/resources/views/COMMON/layout/layout_admin.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="css/adminLayout.css">
+    <link rel="stylesheet" href="css/commonAdmincss.css">
     <script src="{{ asset('js/app.js') }}"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
@@ -14,38 +16,36 @@
     @yield('css')
     <title>@yield('title')</title>
 </head>
+
 <body>
     <div class="container-fluid">
-        
+
         <div class="row">
-          <!-- sidenav bar Start  -->
-          <div class="col-md-2">
-            <div class="sidenav fixed-top">
-              <div
-                class="sidebar-heading text-center py-4 text-uppercase fs-5 text-white"
-              >
-                <li class="adminli">Admin name</li>
-                <li class="adminli">Last Login:12:12</li>
-              </div>
-              <div class="list-group list-group-flush my-3">
-                <a href=""
-                  ><button class="buttons btn active">Dashboard</button></a
-                >
-                <a href=""><button class="buttons btn">Transaction</button></a>
-                <a href=""><button class="buttons btn">Customer</button></a>
-                <a href=""><button class="buttons btn">Coin</button></a>
-                <a href=""><button class="buttons btn">Finace</button></a>
-                <a href=""><button class="buttons btn">Products</button></a>
-                <a href=""><button class="buttons btn">Setting</button></a>
-                <a href=""><button class="logout btn">Logout</button></a>
-              </div>
+            <!-- sidenav bar Start  -->
+            <div class="col-md-2">
+                <div class="sidenav fixed-top">
+                    <div class="sidebar-heading text-center py-4 text-uppercase fs-5 text-white">
+                        <li class="adminli">Admin name</li>
+                        <li class="adminli">Last Login:12:12</li>
+                    </div>
+                    <div class="list-group list-group-flush my-3">
+                        <a href=""><button class="buttons btn active">Dashboard</button></a>
+                        <a href=""><button class="buttons btn">Transaction</button></a>
+                        <a href=""><button class="buttons btn">Customer</button></a>
+                        <a href=""><button class="buttons btn">Coin</button></a>
+                        <a href=""><button class="buttons btn">Finace</button></a>
+                        <a href=""><button class="buttons btn">Products</button></a>
+                        <a href=""><button class="buttons btn">Setting</button></a>
+                        <a href=""><button class="logout btn">Logout</button></a>
+                    </div>
+                </div>
+                <!-- sidenav bar End  -->
             </div>
-            <!-- sidenav bar End  -->
-          </div>
-          <div class="col-md-10">
-            @yield('body')
-          </div>
+            <div class="col-md-10">
+                @yield('body')
+            </div>
         </div>
     </div>
 </body>
+
 </html>
