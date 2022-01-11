050c6d29de35091c45f4f72079418c5bd75512f6
ZNWL <znwinlwin@gmail.com>
2022-01-11 13:21:39 +0630

    From znwl

--- food_lab/resources/views/COMMON/layout/layout_admin.blade.php
+++ food_lab/resources/views/COMMON/layout/layout_admin.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="css/adminLayout.css">
     <script src="{{ asset('js/app.js') }}"></script>
     @yield('script')
     @yield('css')
@@ -12,7 +13,36 @@
 </head>
 <body>
     <div class="container-fluid">
-        @yield('body')
+        
+        <div class="row">
+          <!-- sidenav bar Start  -->
+          <div class="col-md-2">
+            <div class="sidenav fixed-top">
+              <div
+                class="sidebar-heading text-center py-4 text-uppercase fs-5 text-white"
+              >
+                <li class="adminli">Admin name</li>
+                <li class="adminli">Last Login:12:12</li>
+              </div>
+              <div class="list-group list-group-flush my-3">
+                <a href=""
+                  ><button class="buttons btn active">Dashboard</button></a
+                >
+                <a href=""><button class="buttons btn">Transaction</button></a>
+                <a href=""><button class="buttons btn">Customer</button></a>
+                <a href=""><button class="buttons btn">Coin</button></a>
+                <a href=""><button class="buttons btn">Finace</button></a>
+                <a href=""><button class="buttons btn">Products</button></a>
+                <a href=""><button class="buttons btn">Setting</button></a>
+                <a href=""><button class="logout btn">Logout</button></a>
+              </div>
+            </div>
+            <!-- sidenav bar End  -->
+          </div>
+          <div class="col-md-10">
+            @yield('body')
+          </div>
+        </div>
     </div>
 </body>
 </html>
--- food_lab/resources/views/admin/dashboard.blade.php
+++ food_lab/resources/views/admin/dashboard.blade.php
@@ -1,1 +1,19 @@
 @extends('COMMON.layout.layout_admin')
+
+@section('title')
+Dashboard
+@endsection
+
+@section('css')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+@endsection
+
+@section('script')
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+@endsection
+
+
+@section('body')
+<div>asdsadsad</div>
+@endsection
